37972bb0d77708824ab6843b1c9b4164142d0c70
Kipkorir2017 <pkobenj41@gmail.com>
2022-10-11 12:58:28 +0300

    add changes to app.js file and remove some files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import AddContact from "./AdddContact";
+import ContactList from "./ContactList";
 
+class App extends React.Component {
 function App() {
- return (
-  <div>Hello world</div>
+ return ( 
+  <div>
+    <Header />
+    <AddContact />
+    <ContactList />
+  </div>
 )
 }
 
